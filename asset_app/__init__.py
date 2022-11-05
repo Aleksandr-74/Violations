1e5cf38f580f706cb250254aa3e4feacffbb0b07
Aleksandr-74 <74simakovan@gmail.com>
2022-11-05 15:08:43 +0500

    Update __init__.py

--- asset_app/__init__.py
+++ asset_app/__init__.py
@@ -1,20 +1,21 @@
+import os
 
+from dotenv import load_dotenv
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
-
+load_dotenv()
 app = Flask(__name__)
 db = SQLAlchemy()
-
-app.config['SECRET_KEY'] = 'JUSgPqv4hN'
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database.db'
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
 db.init_app(app)
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'auth.login'
-login_manager.login_message='Для доступа пожалуйста авторизуйтесь!'
-login_manager.login_message_category='success'
+login_manager.login_message= 'Для доступа пожалуйста авторизуйтесь!'
+login_manager.login_message_category= 'success'
 
 
 
